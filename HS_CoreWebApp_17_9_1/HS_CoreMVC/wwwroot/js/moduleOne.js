b88c046d834a8f4c0a73fa2209fb2e0340df3031
rgroverRT <rgrover@redtailtechnology.com>
2017-09-20 16:08:03 -0700

    Angular start with MVC project

---
+++ HS_CoreWebApp_17_9_1/HS_CoreMVC/wwwroot/js/moduleOne.js
@@ -0,0 +1,9 @@
+﻿angular.module("coreMvcOne", [])
+    .controller('indexController', function ($scope) {
+    $scope.MessageOne = "1-11-111",
+    $scope.MessageTwo = "22-2222-22222222",
+    $scope.MessageThree = "333",
+    $scope.FunctionOne = function () {
+        window.alert("called FunctionOne, woot.");
+    }
+});
